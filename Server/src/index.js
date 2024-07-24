548b06ff89b5a73a5ce158a364fc126c8e494305
yogit4554 <yt7407286@gmail.com>
2024-07-24 15:59:21 +0530

    added utils

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,12 +1,24 @@
 import 'dotenv/config'
 import {app} from "./app.js"
 import {connectDB} from "./db/index.js"
+import cors from "cors"
 
-connectDB()
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true
+}));
+
+app.use(express.json({limit:"16kb"}))
+
+app.use(express.urlencoded({extended:true,limit:"16kb"}))
+
+app.use(cookieParser())
+
+connectDB() // only code to connect the db
 .then(()=>{
     app.listen(process.env.PORT || 8000,()=>{
         console.log(`Server is running at port: ${process.env.PORT}`)
-    } );
+    });
 })
 .catch((err)=>{
     console.log("MONGO DB connection failed!!",err)
